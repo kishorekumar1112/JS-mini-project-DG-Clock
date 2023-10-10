a597a8bf2dab723eada022835e9a7226bd656f3f
kishorekumar1112 <140399324+kishorekumar1112@users.noreply.github.com>
2023-10-10 21:30:30 +0530

    Add files via upload

---
+++ Digitel Clock/script.js
@@ -0,0 +1,24 @@
+let ampm = document.getElementById('ampm')
+function displayTime(){
+    let dateTime = new Date();
+    let hr = padZero(dateTime.getHours());
+    let min = padZero(dateTime.getMinutes());
+    let sec = padZero(dateTime.getSeconds());
+    if (hr>12){
+        hr = hr - 12;
+        ampm.innerHTML = "PM";
+    }
+    else{
+        ampm.innerHTML = "AM";
+    }
+
+    document.getElementById('hours').innerHTML = padZero(hr);
+    document.getElementById('minutes').innerHTML = padZero(min);
+    document.getElementById('seconds').innerHTML = padZero(sec);
+}
+
+function padZero(num){
+    return num<10?"0"+num:num;
+}
+
+setInterval(displayTime, 500)
